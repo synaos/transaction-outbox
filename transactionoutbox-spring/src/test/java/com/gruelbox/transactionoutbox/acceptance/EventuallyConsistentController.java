98e11ef5c81bde1ca2ae01b86b137d03ba44a681
Dennis Romanowsky <dennis.romanowsky@synaos.com>
2026-06-29 07:58:00 +0200

    [FPOS-43105] Update to Spring Boot 4.1

--- transactionoutbox-spring/src/test/java/com/gruelbox/transactionoutbox/acceptance/EventuallyConsistentController.java
+++ transactionoutbox-spring/src/test/java/com/gruelbox/transactionoutbox/acceptance/EventuallyConsistentController.java
@@ -2,7 +2,7 @@ package com.gruelbox.transactionoutbox.acceptance;
 
 import com.gruelbox.transactionoutbox.TransactionOutbox;
 import java.time.LocalDateTime;
-import javax.inject.Provider;
+import jakarta.inject.Provider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
